9a2acd1f969a7908187773a2fc7ed7e13cf1e1b6
kevinniel <k.niel.pro@gmail.com>
2026-03-10 10:23:20 +0100

    update

--- tests/Unit/CalculatorTest.php
+++ tests/Unit/CalculatorTest.php
@@ -8,13 +8,21 @@ use App\Calculator;
 use PHPUnit\Framework\TestCase;
 use TypeError;
 
+
+/*
+
+Liste des cas qu'on peut tester : 
+- TypeError
+- 
+
+*/
 final class CalculatorTest extends TestCase
 {
     public function testItAddsTwoIntegers(): void
     {
         $calculator = new Calculator();
 
-        self::assertSame(4, $calculator->add(2, 2));
+        self::assertSame(4, $calculator->add(2, null));
         self::assertSame(206000, $calculator->add(101000, 105000));
         self::assertSame(0, $calculator->add(-2, 2));
         self::assertSame(5, $calculator->add(2.25, 2.75));
